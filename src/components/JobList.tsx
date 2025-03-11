60972b7f98e7e1125777ff1a9b573edcaa23072c
Bada Jesutobi <jesutobi_dev@outlook.com>
2025-03-12 00:25:38 +0100

    update

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -8,7 +8,7 @@ type JobListProps = {
 const JobList: React.FC<JobListProps> = ({ jobs }) => {
 
   return (
-    <div className="text-left p-5 lg:p-10 mb-10">
+    <div className="text-left p-5 lg:p-10 mb-30">
       {jobs && jobs.length > 0 ? (
         <ul className="gap-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {jobs.map((job, index) => (
